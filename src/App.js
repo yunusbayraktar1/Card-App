6ddbbfb91e4f34243e850da3fa0159649a66bcfa
YunusBayraktar <123024235+YunusBayraktar@users.noreply.github.com>
2023-10-25 11:35:49 +0300

    sdsaf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import {  Button } from '@mantine/core';
 import './App.css';
-import Card from "./components/Card";
+import CardComponent from "./components/Card";
 import "./components/Card/Card.css";
 import { useState } from "react" //STATE KUTUPHANESİ
 
 let arr = [{ id: 1, tittle: "Dağ 1", par: "Açıklama 1" }, { id: 2, tittle: "Dağ 2", par: "Açıklama 2" },
 { id: 3, tittle: "Dağ 3", par: "Açıklama 3" }, { id: 4, tittle: "Dağ 4", par: "Açıklama 4" }]
 function App() {
-  // kare(5);
   const [ders, setDers] = useState(11);//STATE FONKSİYONU
 
 
@@ -19,10 +18,10 @@ function App() {
       <button onClick={() => setDers(ders - 1)}>Azalt</button>
       <button onClick={() => setDers(ders + 1)}>Arttır</button>
 
-      <div className="Cards">
+      <div className="Card">
         {arr.map(function ({ par, tittle }, i) {
-          <Card par={par} tittle={tittle} />
-          return <Card key={`index ${i}`}
+          <CardComponent par={par} tittle={tittle} />
+          return <CardComponent key={`index ${i}`}
             par={par}
             tittle={tittle}
             ders={ders}
--- src/index.js
+++ src/index.js
@@ -9,15 +9,9 @@ import { MantineProvider } from '@mantine/core';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MantineProvider widthGlobalStyles widthNormalizeCSS>
+    <MantineProvider widthGlobalStyles widthNormalizeCSS >
       <App />
     </MantineProvider>
   </React.StrictMode>
 );
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
